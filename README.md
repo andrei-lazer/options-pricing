20005953ae0cc1bd4af05cc6945335a2949bb6df
Andrei Lazer <andrei.lazer319@gmail.com>
2023-11-08 00:29:17 +0000

    Main edit is the README.md file

--- README.md
+++ README.md
@@ -1,5 +1,43 @@
-# options-pricing
+# Options Pricing
 An exploration of various different options pricing methods in C++.
 
-## European Options Pricing
+## Analytic European Options Pricing
+Here, I have used the common closed form solution for the value of a european option derived from the Black-Scholes equation:
+$$\frac{\partial V}{\partial t} + \frac{1}{2}\sigma S^2 \frac{\partial^2 V}{\partial S^2} + rS\frac{\partial V}{\partial S} - rV = 0$$
+
+Solving this for the boundary conditions of a call option,
+$$
+C(0, t) = 0    \enspace \forall t \\
+C(S, t) \rightarrow S \enspace \text{as} \enspace S \rightarrow \infty \\
+C(S, T) = \operatorname{max}\{S-K, 0\}
+$$
+
+gives the following closed form:
+
+$$
+C(S, t) = N(d_+)S - N(d_-)Ke^{-r(T-t)}
+
+\\[10pt]
+
+d_+ = \frac{1}{\sqrt{T - t}}\left[\ln\left(\frac{S}{K}\right) + \left(r + \frac{\sigma^2}{2}\right)\left(T - t\right)\right] \\[5pt]
+
+d_- = d_+ - \sigma\sqrt{T - t}.
+$$
+
+
+Where $N(x)$ is the c.d.f of the normal distribution.
+
+This first model simply uses this explicit formula to calcuate prices for a call or put European vanilla option. I've done this to acquaint myself with the mathematics behind options pricing, as well as to have a way of testing any future approaches to the same problem.
+
+Below are some test parameters, as well as the predicted call and put prices. All of the time units are in years.
+
+```
+Price of underlying: 100
+Strike price: 100
+Risk-free rate: 0.05
+Volatility: 0.1
+Time to expiry: 1
+Call price: 6.80496
+Put price: 1.9279
+```
 
--- european_options_pricing.cpp
+++ european_options_pricing.cpp
@@ -22,26 +22,14 @@ double put_price(double S, double sigma, double T, double K, double r, double t)
 
 int main()
 {
-    double S = 100.0;  // Option price
+    double S = 100.0;  // Underlying price
     double K = 100.0;  // Strike price
-    double r = 0.05;   // Risk-free rate (5%)
-    double v = 0.2;    // Volatility of the underlying (20%)
-    double T;    // One year until expiry
-    std::cout << "Enter time to expiry:"; std::cin >> T;
-    //std::cout << t;
+    double r = 0.05;   // Risk-free rate
+    double v = 0.1;    // Volatility of the underlying
+    double T;          // One year until expiry
+    std::cout << "Enter time to expiry (in years): "; std::cin >> T;
+
+    std::cout << "\nPrice of underlying: " << S << "\nStrike price: " << K << "\nRisk-free rate: " << r << "\nVolatility: " << v << "\nTime to expiry: " << T << std::endl;
     std::cout << "Call price: " << call_price(S, v, T, K, r, 0) << "\nPut price: " << put_price(S, v, T, K, r, 0) << std::endl;
 
 }
-
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
